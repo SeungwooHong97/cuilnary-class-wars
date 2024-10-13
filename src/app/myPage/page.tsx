1bd9164868226df2632be35fdf860910fc3067d6
hsw <hongsw6934@gmail.com>
2024-10-13 13:57:55 +0900

    feat : zustand, tanstack 셋팅 완료

--- src/app/myPage/page.tsx
+++ src/app/myPage/page.tsx
@@ -1,8 +1,14 @@
 
+import Bookmark from "./_components/Bookmark";
+import Profile from "./_components/Profile";
+
 const myPage = () => {
-    return (
-      <div>myPage</div>
-    )
-  }
-  
-  export default myPage
+  return (
+    <div className="container mx-auto xl:max-w-[1280px]">
+      <Profile></Profile>
+      <Bookmark></Bookmark> 
+    </div>
+  );
+};
+
+export default myPage;
