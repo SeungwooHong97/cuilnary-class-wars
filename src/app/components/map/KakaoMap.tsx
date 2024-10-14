f36d25a633599206d6157cfacb0db7b8ba4e4765
Subin, Han <ella0411@naver.com>
2024-10-14 21:06:55 +0900

    feat: 마커 클릭 시 식당 정보 렌더링

--- src/app/components/map/KakaoMap.tsx
+++ src/app/components/map/KakaoMap.tsx
@@ -2,8 +2,9 @@
 
 import { Restaurant } from "@/types/info";
 import { useEffect, useState } from "react";
-import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { CustomOverlayMap, Map, MapMarker } from "react-kakao-maps-sdk";
 import ReSetttingMapBounds from "./ReSetttingMapBounds";
+import Link from "next/link";
 
 type Props = {
   restaurants: Restaurant[];
@@ -16,6 +17,8 @@ export default function KakaoMap({ restaurants }: Props) {
       lng: number;
     }[]
   >([]);
+
+  const [selectedRestaurant, setSelectedRestaurant] = useState<Restaurant | null>(null);
 
   useEffect(() => {
     const newPoints = restaurants
@@ -35,8 +38,62 @@ export default function KakaoMap({ restaurants }: Props) {
         style={{ width: "800px", height: "800px" }}
         level={13}
       >
-        {points.map((point, index) => (
-          <MapMarker key={`marker__${point.lat}-${point.lng}-${index}`} position={point} />
+        {restaurants.map((rest, index) => (
+          <>
+            <MapMarker
+              key={`marker__${rest.latitude}-${rest.longitude}-${index}`}
+              position={{
+                lat: Number(rest.latitude),
+                lng: Number(rest.longitude)
+              }}
+              clickable={true}
+              onClick={() => setSelectedRestaurant(rest)}
+            />
+            {selectedRestaurant?.restaurant_name === rest.restaurant_name && (
+              <CustomOverlayMap
+                position={{
+                  lat: Number(rest.latitude),
+                  lng: Number(rest.longitude)
+                }}
+                yAnchor={1.4}
+              >
+                <div className="flex flex-col items-center bg-white border rounded-lg shadow-md p-2 w-[200px]">
+                  <div className="flex justify-between items-center w-full mb-2">
+                    <h2 className="text-[13px] font-bold">{rest.restaurant_name}</h2>
+                    <img
+                      alt="close"
+                      width="14"
+                      height="13"
+                      src="https://t1.daumcdn.net/localimg/localimages/07/mapjsapi/2x/bt_close.gif"
+                      className="cursor-pointer"
+                      onClick={() => setSelectedRestaurant(null)}
+                    />
+                  </div>
+                  <div>
+                    <div className="mb-2">
+                      <img
+                        src={rest.restaurant_img_url || ""}
+                        width="73"
+                        height="70"
+                        alt={rest.restaurant_name || "이미지 없음"}
+                      />
+                    </div>
+                    <div>
+                      <div className="flex gap-[10px] items-center">
+                        <p className="text-sm font-light">{rest.address?.split(" ").slice(0, 2).join(" ")}</p>
+                        <Link
+                          href={`https://map.kakao.com/link/search/${encodeURIComponent(rest.restaurant_name)}`}
+                          passHref
+                        >
+                          <span className="text-xs">상세보기</span>
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </CustomOverlayMap>
+            )}
+          </>
         ))}
         {points.length > 0 && <ReSetttingMapBounds points={points} />}
       </Map>
