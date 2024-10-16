82f6ff9701e6b81e406ebbf624b40e04d3fc7e7a
hsw <hongsw6934@gmail.com>
2024-10-16 19:32:46 +0900

    fix error

--- src/app/components/reviews/Reviews.tsx
+++ src/app/components/reviews/Reviews.tsx
@@ -2,14 +2,15 @@
 
 import { Restaurant, Review } from "@/types/info";
 import React, { useState } from "react";
-import ReviewInput from "./ReviewInput";
+
 import { ReviewList } from "./ReviewList";
+import ReviewInput from "./ReviewInput";
 
 const Reviews = ({ rest }: { rest: Restaurant }) => {
   const [reviews, setReviews] = useState<Review[]>([]);
   return (
     <div>
-      <ReviewInput rest={rest} setReviews={setReviews} />
+      <ReviewInput rest={rest}  reviews={reviews} setReviews={setReviews} />
       <ReviewList rest={rest} reviews={reviews} setReviews={setReviews} />
     </div>
   );
