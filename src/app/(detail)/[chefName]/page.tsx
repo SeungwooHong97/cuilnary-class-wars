504801d8d65154fa3dbb149d4121793b7dde5342
Subin, Han <ella0411@naver.com>
2024-10-16 23:49:45 +0900

    style: chefDetail CSS

--- src/app/(detail)/[chefName]/page.tsx
+++ src/app/(detail)/[chefName]/page.tsx
@@ -42,22 +42,30 @@ const ChefDetailPage = ({ params }: Props) => {
 
   if (loading) {
     return (
-      <div className="flex justify-between items-center min-h-[calc(100vh-72px)] bg-gray-50">
-        <div className="w-[550px] h-[800px] rounded-r-lg bg-zinc-200 mt-[30px]" />
-        <div className="mr-[20px] w-[1100px] h-[800px] bg-zinc-200 mt-[30px]" />
+      <div className="flex sm:flex-col md:flex-row justify-center items-center min-h-[calc(100vh-72px)] bg-gray-50">
+        <div className="flex-none items-center w-full md:w-[550px] mr-[20px]">
+          <div className="w-full md:w-[550px] h-[400px] md:h-[800px] rounded-r-lg bg-zinc-200 mt-[30px]" />
+        </div>
+        <div className="flex-grow">
+          <div className="mr-[20px] w-full h-[400px] md:h-[800px] bg-zinc-200 mt-[30px]" />
+        </div>
       </div>
     );
   }
 
   if (chefData)
     return (
-      <div className="flex justify-center items-center min-h-[calc(100vh-72px)] bg-gray-50">
-        <ChefDetail chefData={chefData} restaurants={restaurants} handleMoveToLocation={handleMoveToLocation} />
-        <KakaoMap
-          restaurants={restaurants}
-          selectedLocation={selectedLocation}
-          size={{ width: "1100px", height: "800px" }}
-        />
+      <div className="flex sm:flex-col md:flex-row justify-center items-center min-h-[calc(100vh-72px)] bg-gray-50">
+        <div className="flex-none items-center w-full md:w-[550px] mr-[20px]">
+          <ChefDetail chefData={chefData} restaurants={restaurants} handleMoveToLocation={handleMoveToLocation} />
+        </div>
+        <div className="flex-grow">
+          <KakaoMap
+            restaurants={restaurants}
+            selectedLocation={selectedLocation}
+            size={{ width: "100%", height: "800px" }}
+          />
+        </div>
       </div>
     );
 };
