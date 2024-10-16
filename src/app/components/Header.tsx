f8fbe3fb5d456cd68198156dfbab711b167dd600
hsw <hongsw6934@gmail.com>
2024-10-16 13:47:13 +0900

    feat : css , header 수정 진행중:

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,7 +3,7 @@
 import useAuthStore from "../../../zustand/userStore";
 import Link from "next/link";
 import { logout } from "@/utils/supabase/supabaseApi";
-import ChefLogo from "../../../../public/icons/chef-hat-svgrepo-com.svg";
+import ChefLogo from "../../../public/icons/chef-hat-svgrepo-com.svg";
 
 const Header = () => {
   const { isLoggedIn, setClearAuth } = useAuthStore();
@@ -16,9 +16,10 @@ const Header = () => {
   return (
     <header className="bg-white shadow-md w-full fixed top-0 left-0 z-50">
       <nav className="container mx-auto px-6 py-4 flex justify-between items-center">
-        <ChefLogo>
-          <Link href="/" className="font-bold text-black text-lg hover:text-gray-700"></Link>
-        </ChefLogo>
+        <Link href="/">
+          <ChefLogo className="w-10 h-10 cursor-pointer"></ChefLogo>
+        </Link>
+
         <div className="flex items-center space-x-6">
           {isLoggedIn ? (
             <>
